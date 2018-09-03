a69192c54627c70881702ca641a964f60b763b65
anands-cuelogic <anand.singh@cuelogic.com>
2018-09-03 19:30:03 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -6,14 +6,34 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 const io = socketIO(server);
 
 io.on("connection", (socket) => {
-    console.log('New user connected');
+
+    socket.emit('userConnected', {
+        from: 'admin',
+        text: 'Welcome to the chat app'
+    });
+    socket.broadcast.emit('userConnected', {
+        from: 'admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.on('createEmail', (message) => {
+    //     console.log('createEmail ', message);
+
+    //     socket.broadcast.emit('newMessage', {
+    //         from: message.from,
+    //         to: message.to,
+    //         text: message.text,
+    //         createdAt: new Date().getTime
+    //     });
+    // });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
